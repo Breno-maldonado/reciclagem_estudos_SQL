9607707c225ababb41421c1c73bcb98bea89fb4d
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-08 18:01:23 -0300

    aula 4: ALTER TABLE / DROP TABLE / DROP SCHEMA

---
+++ aula_4.sql
@@ -0,0 +1,9 @@
+ALTER TABLE -- alterar tabela
+ALTER TABLE tabelaclientes -- seleciona a tabela a qual vai alterar
+ALTER TABLE tabelaclientes ADD Endereco_Cliente VARCHAR (250); -- vai adicionar uma nova coluna e passando as especificações dela
+
+DROP TABLE -- vai excluir uma tabela
+DROP TABLE tabelaclientes -- para excluir uma tabela especifica, você passa o nome da mesma
+
+DROP SCHEMA -- vai excluir um banco de dados inteiro
+DROP SCHEMA db_financas -- para excluir um banco de dados especifico
