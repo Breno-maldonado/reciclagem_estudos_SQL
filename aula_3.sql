fd25afcb2cc3389fd47ace8b5a89adc03cbb446f
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-08 17:14:30 -0300

    aula 3: CREATE TABLE

---
+++ aula_3.sql
@@ -0,0 +1,3 @@
+CREATE TABLE -- inicio para criar uma tabela
+CREATE TABLE tabelacliente -- cria o nome da tabela
+CREATE TABLE tabelacliente (ID_Cliente INT PRIMARY KEY) -- cria a coluna "ID_Cliente" passa o tipo dela "INT" e atribui a ela a chave primaria "PRIMARY KEY"
