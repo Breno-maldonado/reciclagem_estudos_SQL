a640b0d05ca715bdbe0f36634a0de21b8684c8fd
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-08 15:57:17 -0300

    aula 1: SELECT * FROM

---
+++ aula_1.sql
@@ -0,0 +1,6 @@
+SELECT -- seleciona
+SELECT * -- seleciona tudo
+SELECT * FROM -- seleciona tudo de uma tabela
+SELECT * FROM tb_financeiro -- seleciona tudo da tabela "tb_financeiro"
+
+SELECT * FROM tb_financeiro; -- para boas praticas é interessante utilizar o ponto e virgula (;) no final da consulta
