a121c144b80eff41cb176ad41470f05544191b96
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-09 18:54:36 -0300

    aula 6: INSERT INTO & VALUES

---
+++ aula_6.sql
@@ -0,0 +1,11 @@
+INSERT INTO -- seleciona as tabelas e colunas que ira receber dados
+VALUES -- insere os valores seguindo a ordem especificada no layout 
+
+INSERT INTO tabelaclientes(   
+    id_cliente,
+    nome_cliente,
+    informacoes_contato,
+    endereco_cliente
+)
+VALUES
+    ('1','Breno Maldonado', 'breno.maldonado@email.com', 'rua groove street 1')
