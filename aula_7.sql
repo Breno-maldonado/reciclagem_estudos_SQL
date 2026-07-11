d4356eec053a6996f8962e3e4b718a9c4b1ec65b
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-10 21:12:43 -0300

    aula 7: Operadores

---
+++ aula_7.sql
@@ -0,0 +1,7 @@
+SELECT * FROM tabela WHERE dado = 1 -- procura o dado que seja igual a 1
+SELECT * FROM tabela WHERE dado != ou <> 1 -- procura o dado que seja diferente de 1
+SELECT * FROM tabela WHERE dado == 1 -- procura o dado que seja exatamente 1
+SELECT * FROM tabela WHERE dado <= 1 -- procura o dado que seja menor ou igual a 1
+SELECT * FROM tabela WHERE dado >= 1 -- procura o dado que seja maior ou igual a 1
+SELECT * FROM tabela WHERE dado < 1 -- procura o dado que seja menor que 1
+SELECT * FROM tabela WHERE dado > 1 -- procura o dado que seja maior que 1
