5657cb3460eb04474a0dfedcde93cf6adf715fc9
Breno Maldonado <maldonadobreno12@hotmail.com>
2026-07-11 21:25:19 -0300

    aula 10: UPDATE / SET

---
+++ aula_10.sql
@@ -0,0 +1,6 @@
+UPDATE -- ira realizar uma alteração na tabela indicada
+SET -- vai definir a coluna
+
+UPDATE tabela -- vai indicar a tabela
+SET coluna = 'contrato' -- vai indicar a coluna + o valor de alteração
+WHERE coluna = 'cadastro' -- onde o valor vai ser aplicado
